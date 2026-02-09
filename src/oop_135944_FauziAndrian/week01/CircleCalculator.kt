310b1f86ac1d7ec4c8c288630d74f4c962526f8d
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-09 13:14:12 +0700

    week01: refactor to string template and type inference

--- src/oop_135944_FauziAndrian/week01/CircleCalculator.kt
+++ src/oop_135944_FauziAndrian/week01/CircleCalculator.kt
@@ -2,14 +2,14 @@ package oop_001_johnthor.week01
 
 fun main(args: Array<String>) {
     // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius = 7.0
+    val pi = 3.14
 
     // Calculation
-    var area: Double = pi * radius *radius
+    val area = pi * radius *radius
 
     // Output Cocetenation
-    println("Radius: " + radius +", Area: " + area)
+    println("Radius: $radius, Area: $area")
 
     // Logic check
     checkSize(area)
