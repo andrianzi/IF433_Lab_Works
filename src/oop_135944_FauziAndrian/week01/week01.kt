93726e47b7273d2a54c8dda34b5435673cae8b45
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-09 11:48:54 +0700

    week01: initial setup and procedural code

---
+++ src/oop_135944_FauziAndrian/week01/week01.kt
@@ -0,0 +1,9 @@
+package oop_135944_FauziAndrian.week01
+
+fun main() {
+    var name: String = "john thor"
+    var score: Int = 80
+
+    // Concentanation gaya lama
+    println("Nama: " +  ", Nilai: " + score)
+}
