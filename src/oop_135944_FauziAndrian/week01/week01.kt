5537d4683a416da74e54ba9ef6281ad4988b475d
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-09 11:52:42 +0700

    week01: refactor to use val and string templates

--- src/oop_135944_FauziAndrian/week01/week01.kt
+++ src/oop_135944_FauziAndrian/week01/week01.kt
@@ -1,9 +1,9 @@
 package oop_135944_FauziAndrian.week01
 
 fun main() {
-    var name: String = "john thor"
-    var score: Int = 80
+    val name: String = "john thor"
+    val score: Int = 80
 
     // Concentanation gaya lama
-    println("Nama: " +  ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
