8a84ec59e9b7532b988e38a7f25ff15f8f0eba8c
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-09 12:02:31 +0700

    week01: add calculateStatus function

--- src/oop_135944_FauziAndrian/week01/week01.kt
+++ src/oop_135944_FauziAndrian/week01/week01.kt
@@ -17,4 +17,9 @@ fun main() {
 
     println("Grade kamu: $grade")
 
+    println("Status: ${calculateStatus(score)}")
+
+
 }
+
+fun  calculateStatus(score: Int) = if (score < 75) "Lulus" else "Tidak lulus"
