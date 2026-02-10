c30d039e43fbed0560601902cad474f75936b71c
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-10 15:31:15 +0700

    week02: create basic Student class structure

---
+++ src/Student.kt
@@ -0,0 +1,9 @@
+package oop_135944_FauziAndrian.week02
+
+class Student(
+    val name: String,
+    val nim: String,
+    val major: String
+){
+    // Body class kosong
+}
---
+++ src/latihan.kt
@@ -0,0 +1,55 @@
+package oop_135944_FauziAndrian.week02
+
+/*
+class MyCar {
+    public var warna: String = "abu"
+
+    fun kecepatan() {
+        println("kecepatannya $warna nya luar biasa")
+    }
+}
+
+fun main() {
+    val myCar = MyCar();
+    myCar.kecepatan();
+
+    myCar.warna = "metalik";
+
+    println("warna mobil ${myCar.warna}")
+}
+
+*/
+/*
+class MyCar {
+    public var warna: String = "abu"
+    public var kecepatan: Int = 70;
+
+    init {
+        if (kecepatan>=70) {
+            println("fasthand")
+        } else {
+            println("unfasthand")
+        }
+    }
+
+    constructor(speed: Int=0, brake: Double=1.3){
+        this.kecepatan = speed
+        println("ini dari si secondary constr $kecepatan dan $brake")
+    }
+
+    fun mulai_jalan() {
+        println("kecepatannya $warna nya luar biasa")
+    }
+}
+
+fun main() {
+    val myCar = MyCar(100, 5.4);
+    myCar.mulai_jalan();
+
+    myCar.warna = "metalik";
+
+    println("warna mobil ${myCar.warna}")
+}
+
+*/
+
