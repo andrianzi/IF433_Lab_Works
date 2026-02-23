4d81b1f589a40e74b2188896243074f14c859d2c
Fauzi Andrian <ojizzi164@gmail.com>
2026-02-23 22:45:03 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/week03/Player.kt
@@ -0,0 +1,20 @@
+package oop_135944_FauziAndrian.week02.week03
+
+class Player(val username: String) {
+
+    private var xp = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) return
+
+        val oldLevel = level
+        xp += amount
+
+        if (level > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $level!")
+        }
+    }
+}
