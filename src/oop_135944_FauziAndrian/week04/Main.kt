4965e89c076fbc8de7ddb4f978795cd1f32b8d1c
Fauzi Andrian <ojizzi164@gmail.com>
2026-03-02 23:38:15 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_135944_FauziAndrian/week04/Main.kt
@@ -0,0 +1,16 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week04
+
+import oop_135944_FauziAndrian.week04.Vehicle
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle( brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car( brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
