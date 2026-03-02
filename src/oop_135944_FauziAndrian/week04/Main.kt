ace103908312fce0daef36f66bcd0194bb168253
Fauzi Andrian <ojizzi164@gmail.com>
2026-03-02 23:54:32 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_135944_FauziAndrian/week04/Main.kt
+++ src/oop_135944_FauziAndrian/week04/Main.kt
@@ -1,7 +1,7 @@
 package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week04
 
 import oop_135944_FauziAndrian.week04.Vehicle
-
+/*
 fun main() {
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle( brand = "Sepeda Onthel")
@@ -13,4 +13,15 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+}
+
+ */
+
+fun main() {
+
+    val car = ElectricCar("Tesla", 100)
+
+    car.accelerate()
+    car.honk()
+    car.openTrunk()
 }
