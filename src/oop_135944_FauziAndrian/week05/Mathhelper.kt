d8c7e7e3c0fd39194c1d1bf51d5908c34c200ce9
Fauzi Andrian <ojizzi164@gmail.com>
2026-03-09 22:05:03 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_135944_FauziAndrian/week05/Mathhelper.kt
@@ -0,0 +1,18 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week05
+
+package oop_135944_FauziAndrian.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
