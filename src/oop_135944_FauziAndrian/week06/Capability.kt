0b86500cbc707d408c394b64e18abf2bbd1c2cdc
Fauzi Andrian <ojizzi164@gmail.com>
2026-03-10 15:27:07 +0700

    week06: create compound interfaces

---
+++ src/oop_135944_FauziAndrian/week06/Capability.kt
@@ -0,0 +1,9 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week06
+
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun rechargeBattery()
+}
