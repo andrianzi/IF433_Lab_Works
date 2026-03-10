d58dcaccff85b606cee82c0cb05a68118291ddb9
Fauzi Andrian <ojizzi164@gmail.com>
2026-03-10 15:37:03 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_135944_FauziAndrian/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("sinyal seluler mencari jaringan.") }
+}
