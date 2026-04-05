54c3ad6fde2fef64274549de9f9829369e4b1909
Fauzi Andrian <ojizzi164@gmail.com>
2026-04-05 23:28:13 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_135944_FauziAndrian/week07/GameExecutor.kt
@@ -0,0 +1,25 @@
+package oop_135944_FauziAndrian.week07
+
+fun processEvent(event: BattleState) {
+
+    when (event) {
+
+        is BattleState.MonsterEncounter -> {
+            println("⚔️ Monster muncul: ${event.monsterName}")
+        }
+
+        is BattleState.LootDropped -> {
+            // destructuring
+            val (name, damage, rarity) = event.item
+            println("🎁 Loot didapat: $name (Damage: $damage, Rarity: $rarity)")
+        }
+
+        is BattleState.GameOver -> {
+            println("💀 Game Over: ${event.reason}")
+        }
+
+        is BattleState.SafeZone -> {
+            println("🛡️ Kamu berada di zona aman")
+        }
+    }
+}
