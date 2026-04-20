f0e7dc1018c13cecb09b975a61f9c3b30e4bc1d1
Fauzi Andrian <ojizzi164@gmail.com>
2026-04-21 00:40:03 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_135944_FauziAndrian/week08/EcommersModels.kt
@@ -0,0 +1,15 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
