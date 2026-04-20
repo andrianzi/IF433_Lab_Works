e78fd914561349ec4245f4b6d7aaa50eabdb1843
Fauzi Andrian <ojizzi164@gmail.com>
2026-04-21 00:04:44 +0700

    week08: create fallback logic with as? and elvis

--- src/oop_135944_FauziAndrian/week08/Main.kt
+++ src/oop_135944_FauziAndrian/week08/Main.kt
@@ -37,4 +37,8 @@ fun main() {
             println("Ditemukan teks: ${it.uppercase()}")
         }
     }
+
+    val someObject: Any = 100 // Tipe aslinya Integer
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast + fallback: $safeString")
 }
