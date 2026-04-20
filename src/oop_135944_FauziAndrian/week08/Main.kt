837b3218c56b5875f39d32f41600aa74a5251693
Fauzi Andrian <ojizzi164@gmail.com>
2026-04-20 23:54:56 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_135944_FauziAndrian/week08/Main.kt
@@ -0,0 +1,11 @@
+package oop_135944_FauziAndrian.week02.oop_135944_FauziAndrian.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // Rantai Safe Calls yang elegan
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+
+    println("Tujuan pengiriman: $destination")
+}
